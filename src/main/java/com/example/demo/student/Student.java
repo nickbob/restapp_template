df6a6cca2ac884c227258b30e7f496edf5b1c614
Сергей Александрович Бобкин <sabobkin.sbt@sberbank.ru>
2021-04-12 17:19:21 +0300

    nickbob, add post request api

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,6 +2,7 @@ package com.example.demo.student;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table
@@ -20,6 +21,26 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
+
+    public Integer getAge() {
+        return Period.between(this.dob, LocalDate.now()).getYears();
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
     public Student() {
     }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.Optional;
 
 
 @Service
@@ -22,5 +22,15 @@ public class StudentService {
     public List<Student> getStudents() {
 //        return Arrays.asList(new Student(1L, "Mike", "mike@gmail.com", LocalDate.of(2000, 12, 12)));
         return studentRepository.findAll();
+    }
+
+    public void add(Student student) {
+        Optional<Student> optionalStudent = studentRepository.findStudentByEmail(student.getEmail());
+
+        if (optionalStudent.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+
+        studentRepository.save(student);
     }
 }
